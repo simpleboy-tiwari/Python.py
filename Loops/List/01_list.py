00547b090e420a10bf93487ae29209e95f427596
simpleboy-tiwari <pushkart986@gmail.com>
2026-04-19 15:52:45 +0530

    List

---
+++ Loops/List/01_list.py
@@ -0,0 +1,39 @@
+# List 
+marks = [94.4,92.9,83.4,95.1,90.4]
+print(marks)
+print(len(marks))
+print(type(marks))
+print(marks[0])
+print(marks[3])
+
+student =["Pushkar",94.5,41,"Bihar"]
+print(student[0])
+student[0] = "Vishu"
+print(student)
+print(marks[1:3])
+print(student[1:4])
+
+list = [2,5,3,6,1]
+
+list.append(2)
+print(list)
+
+list.sort()
+print(list)
+
+list.reverse()
+print(list)
+
+list.insert(3,10)
+print(list)
+
+list = ["a","c","z","x"]
+
+list.append("h")
+print(list)
+
+list.sort()
+print(list)
+
+list.reverse()
+print(list)
